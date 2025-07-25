976203271cc35cc5dffe3c59852dc566f1fba224
Иван Якимов <sasha2020419@gmail.com>
2025-07-25 12:52:31 +0300

    fix link middlware

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { register, login, refreshToken, logout, getMe } = require('../controllers/authController');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/register', register);
 router.post('/login', login);

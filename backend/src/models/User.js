33a0a16db6489ce0e8cd5c5ed77b15cae5c946e3
Иван Якимов <sasha2020419@gmail.com>
2025-07-28 21:35:00 +0300

    add comparePassword method

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new mongoose.Schema({
   username: { 
@@ -25,6 +26,18 @@ const UserSchema = new mongoose.Schema({
   }
 }, { 
   timestamps: true 
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 export default mongoose.model('User', UserSchema);

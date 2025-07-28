e9227a5f2405ab3292970d9e1a150449b9997927
Иван Якимов <sasha2020419@gmail.com>
2025-07-28 21:39:16 +0300

    fix userSchema

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 
+// Создаем схему
 const UserSchema = new mongoose.Schema({
   username: { 
     type: String, 
@@ -28,11 +29,8 @@ const UserSchema = new mongoose.Schema({
   timestamps: true 
 });
 
-userSchema.methods.comparePassword = async function(candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
-};
-
-userSchema.pre('save', async function(next) {
+// Хук для хеширования пароля перед сохранением
+UserSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   
   const salt = await bcrypt.genSalt(10);
@@ -40,4 +38,11 @@ userSchema.pre('save', async function(next) {
   next();
 });
 
-export default mongoose.model('User', UserSchema);
+// Метод для сравнения паролей
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+// Создаем и экспортируем модель
+const User = mongoose.model('User', UserSchema);
+export default User;
